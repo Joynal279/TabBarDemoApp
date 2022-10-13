e65efd6426ed6bfccb6c71997ce7cb5b5e3dfa44
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-10-13 08:57:23 +0600

    Initial Commit

---
+++ TabBarDemoApp/TabBarDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarDemoAppApp.swift
+//  TabBarDemoApp
+//
+//  Created by JOYNAL ABEDIN on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TabBarDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
